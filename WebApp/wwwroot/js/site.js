962a70f8b5a892d2d8539cd7834c3f5330ea03ad
Keymil72 <113836@student.san.edu.pl>
2023-01-21 00:40:35 +0100

    Dodaj pliki projektów.

---
+++ WebApp/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function refreshTime() {
+    const timeDisplay = document.getElementById("today justify-content-end");
+    const dateString = new Date().toLocaleString();
+    const formattedString = dateString.replace(", ", " godz ");
+    timeDisplay.textContent = formattedString;
+}
+setInterval(refreshTime, 1000);
